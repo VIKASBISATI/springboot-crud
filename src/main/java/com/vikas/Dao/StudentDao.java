6c81f56310f0886b12e6fc711283906bda0969b6
harishankarpandit <harishankar.pandit97@gmail.com>
2020-05-20 21:44:25 +0530

    crud springboot

---
+++ src/main/java/com/vikas/Dao/StudentDao.java
@@ -0,0 +1,38 @@
+package com.vikas.Dao;
+
+import com.vikas.Entity.Student;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class StudentDao {
+    private static Map<Integer, Student> students;
+    static {
+        students = new HashMap<Integer, Student>(){{
+            put(1, new Student(1,"John", "Information Technology"));
+            put(2, new Student(2,"David", "Finance"));
+            put(3, new Student(3,"Sai", "Mechanical"));
+        }};
+    }
+    public Collection<Student> getAllStudents(){
+        return this.students.values();
+    }
+    public Student getStudentById(int id) {
+        return this.students.get(id);
+    }
+    public void removeStudentById(int id) {
+         this.students.remove(id);
+    }
+    public void updateStudent(Student student) {
+        Student s = students.get(student.getId());
+        s.setCourse(student.getCourse());
+        s.setName(student.getName());
+        students.put(student.getId(), student);
+    }
+    public void insertStudent(Student student) {
+        students.put(student.getId(), student);
+    }
+}
---
+++ src/main/java/com/vikas/Main.java
@@ -0,0 +1,12 @@
+package com.vikas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        System.setProperty("server.port", "8081");
+        SpringApplication.run(Main.class, args);
+    }
+}
